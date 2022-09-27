5095c98950085950b4f41cf4d47803fed7b727cc
nelcolon <ncolonmeran@gmail.com>
2022-09-27 17:10:58 -0400

    adding modification for starting to work with dictionaries.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        // Title from button coming from.
+        
+        let hardness = sender.currentTitle
+        print(eggTimes[hardness!] ?? "Error")
+        
+    }
+    
 }
